546c7b1b7286c5ba59156a74cd7d058c5dda0698
989onan <onanchew@gmail.com>
2024-02-01 13:44:37 -0500

    thing
    github wiped all my progress, here's what I have after 2 weeks.

--- canofmystery/app/page.js
+++ canofmystery/app/page.js
@@ -1,6 +1,8 @@
 import styles from "./homepage.module.css";
 import Lander from "../components/lander/Lander"
 import DisplayTextComp from "@/components/displayTextComp/DisplayTextComp";
+import Components from "../components/DisplayComp/Components";
+import RecentSection from "../components/RecentSection/RecentSection";
 
 
 export default function Home() {
@@ -13,6 +15,7 @@ export default function Home() {
         {/* <div className="h-max dark:bg-base-100-dark text-t-header-light bg-base-100">
           <DisplayTextComp />
         </div> */}
+        <RecentSection/>
         {/* <Components /> */}
     </div>
   )
