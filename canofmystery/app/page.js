02792e6cfb86c53d07b1ed6d06a3fede3f0b8de8
989onan <onanchew@gmail.com>
2024-02-04 18:51:47 -0500

    add the start to a categories section

--- canofmystery/app/page.js
+++ canofmystery/app/page.js
@@ -1,17 +1,16 @@
 import styles from "./homepage.module.css";
 import Lander from "../components/lander/Lander"
 import DisplayTextComp from "@/components/displayTextComp/DisplayTextComp";
-
+import CategoriesSection from "../components/categories/categories";
 
 export default function Home() {
   return (
     <div className={styles.container}>
         <Lander/>
-        <div className="w-full h-[500px]">
-
-        </div>
+        <CategoriesSection/>
         {/* <div className="h-max dark:bg-base-100-dark text-t-header-light bg-base-100">
           <DisplayTextComp />
+        
         </div> */}
         {/* <Components /> */}
     </div>
