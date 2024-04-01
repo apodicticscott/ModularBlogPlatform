a72666fca3ac6c7a3248d26979c76d66251d67ee
Awalter7 <wildx947@gmail.com>
2024-03-31 22:51:13 -0400

    another attempt

--- canofmystery/next.config.js
+++ canofmystery/next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-      nftTracing: true,
-    },
-    images: {
-      domains: [],
-    },
-    reactStrictMode: true,
-  }
-  
-  module.exports = nextConfig
+const nextConfig = {}
+
+module.exports = nextConfig
